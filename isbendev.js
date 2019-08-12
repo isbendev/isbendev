15ff46e609e36d2c1454e30ce55881063330c1d4
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-12 22:29:26 +0100

    Add files via upload

---
+++ isbendev.js
@@ -0,0 +1,17 @@
+<script>
+  $('.desc').css('margin','auto');
+  $('.desc').css('font-size','50px');
+  $('.desc').css('font-family','fantasy');
+  $('.desc').css('color','white');
+  $('.copyright').css('font-size','20px');
+  $('.copyright').css('font-family','fantasy');
+  $('.copyright').css('color','white');
+
+$( document ).ready(function() {
+  html2canvas(document.querySelector('#pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
+    onrendered: function(canvas) {
+      $('#pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
+    }
+  });
+});
+</script>
