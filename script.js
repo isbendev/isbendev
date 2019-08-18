462bd3b430685c96ef74a727995c67bca5e88735
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-18 17:18:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
+window.addEventListener("load", pageFullyLoaded, false);
+
+function theDomHasLoaded(e) {
+  
 var style=document.createElement("link");
 style.setAttribute("rel", "stylesheet");
 style.setAttribute("type", "text/css");
@@ -13,12 +20,14 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas);
+  
+}
 
-setTimeout(function(){
-html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
+function pageFullyLoaded(e) {
+    html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
   onrendered: function(canvas) {
     $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
   }
-});
-}, 1000);
+  });
+}
 
