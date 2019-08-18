6b874d1305457736def0d108015e4ce442bf7129
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-18 11:58:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function ignoreerror(){ return true }
+window.onerror=ignoreerror();
+
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://superal.github.io/canvas2image/canvas2image.js");
