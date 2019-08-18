c182ae9f44b3e7d6071787d904ae8ceb1453a303
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-18 13:16:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var style=document.createElement("link");
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("type", "text/css");
+style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
+document.getElementsByTagName("head")[0].appendChild(style);
+
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://superal.github.io/canvas2image/canvas2image.js");
@@ -7,12 +13,6 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas);
-
-var style=document.createElement("link");
-style.setAttribute("rel", "stylesheet");
-style.setAttribute("type", "text/css");
-style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
-document.getElementsByTagName("head")[0].appendChild(style);
 
 $( document ).ready(function() {
   html2canvas(document.querySelector('#pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
