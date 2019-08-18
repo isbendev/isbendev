f58973893d1a3de1d6ba4917757dc93b9f172227
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-18 16:16:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ style.setAttribute("rel", "stylesheet");
 style.setAttribute("type", "text/css");
 style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
 document.getElementsByTagName("head")[0].appendChild(style);
-
+/*
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js");
@@ -13,7 +13,7 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.3/dist/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas);
-
+*/
 document.addEventListener('DOMContentLoaded', function(){
   html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
     onrendered: function(canvas) {
