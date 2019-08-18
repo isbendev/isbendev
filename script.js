7e707a5bf7c86b81c162ec2b02017ed570a6b8c2
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-18 12:25:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function ignoreerror(){ return true }
-window.onerror=ignoreerror();
-
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://superal.github.io/canvas2image/canvas2image.js");
@@ -10,8 +7,6 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas);
-
-$('.pinpin').html("<div id='pin' style='height:900px; width: 600px; margin: auto; margin-top:30px;'><div style='height:600px; background-color: blue; width: 600px;'><img class='post-thumbnail' crossorigin='anonymous' expr:src='"<data:blog.postImageUrl/>"' height='600px' id='img1' width='600px'/> </div><div style='height:260px; background-color: red; width: 600px; position: relative;'><div class='desc' style='margin: 0; position: absolute; top: 50%; -ms-transform: translateY(-50%); transform: translateY(-50%); text-align: center; left: 10%; right: 10%;'><data:blog.pageName/></div></div><div style='height:40px; background-color: red; width: 600px; text-align: center'><div class='copyright'><data:blog.homepageUrl/> &#9400;</div></div></div>");
 
   $('.desc').css('margin','auto');
   $('.desc').css('font-size','50px');
