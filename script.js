071a3a24eced9f294b426ef570f7d3af1db0e132
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-20 19:45:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
 document.getElementsByTagName("head")[0].appendChild(style);
 
 $('article').after("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
-$('.pinimg1').text($('.img-url').text());
-$('.desctxt').text($('.img-url').text());
-$('.copyrighttxt').text($('.img-url').text());
+$('.pinimg1').attr('src',$('.img-url').text());
+$('.desctxt').text($('.post-title').text());
+$('.copyrighttxt').text($('.site-url').text());
   
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
