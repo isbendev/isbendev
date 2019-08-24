81cd03d35f52abdd35284b95708acd88d1326e5f
isbendev <54044125+isbendev@users.noreply.github.com>
2019-08-24 23:35:22 +0100

    Update scriptwp.js

--- scriptwp.js
+++ scriptwp.js
@@ -9,7 +9,7 @@ style.setAttribute("type", "text/css");
 style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
 document.getElementsByTagName("head")[0].appendChild(style);
 
-$('article').after("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
+$('aside').after("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
 $('.pinimg1').attr('src',$('.img-url').text());
 $('.desctxt').text($('.article-title').text());
 $('.copyrighttxt').text($('.site-url').text());
@@ -29,7 +29,7 @@ document.getElementsByTagName("head")[0].appendChild(html2canvas);
 function pageFullyLoaded(e) {
     html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
   onrendered: function(canvas) {
-    $('#sidebar img').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
+    $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
   }
   });
 }
