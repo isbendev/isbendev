7999c524f83c7f51854d07f4ff38a2e9651ba91f
isbendev <54044125+isbendev@users.noreply.github.com>
2019-09-01 21:00:37 +0100

    Update scriptv2.js

--- scriptv2.js
+++ scriptv2.js
@@ -1,3 +1,9 @@
+
+$('article').after("<div class='pin' id='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
+$('.pinimg1').attr('src',$('.img-url').text());
+$('.desctxt').text($('.article-title').text());
+$('.copyrighttxt').text($('.site-url').text());
+
 $( document ).ready(function() {
   
 var style=document.createElement("link");
@@ -5,23 +11,16 @@ style.setAttribute("rel", "stylesheet");
 style.setAttribute("type", "text/css");
 style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
 document.getElementsByTagName("head")[0].appendChild(style);
-
-$('article').after("<div class='pin' id='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
-$('.pinimg1').attr('src',$('.img-url').text());
-$('.desctxt').text($('.article-title').text());
-$('.copyrighttxt').text($('.site-url').text());
+  
+var html2canvas=document.createElement('script');
+html2canvas.setAttribute("type","text/javascript");
+html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
+document.getElementsByTagName("head")[0].appendChild(html2canvas);
   
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js");
 document.getElementsByTagName("head")[0].appendChild(canvas2image);
-
-var html2canvas=document.createElement('script');
-html2canvas.setAttribute("type","text/javascript");
-html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
-document.getElementsByTagName("head")[0].appendChild(html2canvas);
- 
-
   
 html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
   onrendered: function(canvas) {
