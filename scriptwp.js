7dd73af95f11cd3852cd3ce5fea37e2f1a207207
isbendev <54044125+isbendev@users.noreply.github.com>
2019-09-05 19:25:40 +0100

    Update scriptwp.js

--- scriptwp.js
+++ scriptwp.js
@@ -1,17 +1,8 @@
-document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
-window.addEventListener("load", pageFullyLoaded, false);
-
-function theDomHasLoaded(e) {
 var style=document.createElement("link");
 style.setAttribute("rel", "stylesheet");
 style.setAttribute("type", "text/css");
 style.setAttribute("href", "https://isbendev.github.io/isbendev/style.css");
 document.getElementsByTagName("head")[0].appendChild(style);
-
-$('article').after("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
-$('.pinimg1').attr('src',$('.img-url').text());
-$('.desctxt').text($('.article-title').text());
-$('.copyrighttxt').text($('.site-url').text());
   
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
@@ -22,13 +13,20 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas); 
-}
 
-function pageFullyLoaded(e) {
-    html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
+$('article').after("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
+$('.pinimg1').attr('src',$('.img-url').text());
+$('.desctxt').text($('.article-title').text());
+$('.copyrighttxt').text($('.site-url').text());
+
+
+$( document ).ready(function() { 
+  
+  html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
   onrendered: function(canvas) {
     $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
-  }
+    }
   });
-}
+  
+});
 
