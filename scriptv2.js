f9e52070598b7f3510ea88cf3491a2eee8514b95
isbendev <54044125+isbendev@users.noreply.github.com>
2019-09-19 19:33:18 +0100

    Update scriptv2.js

--- scriptv2.js
+++ scriptv2.js
@@ -7,7 +7,7 @@ $('.copyrighttxt').text($('.site-url').text());
   
 html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
   onrendered: function(canvas) {
-    //$('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
+    $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
     //Canvas2Image.saveAsJPEG(canvas, 600, 600);
     //$('.pin').after("<a class='pin-art' href='http://pinterest.com/pin/create/button/?url=" + window.location.href + "&amp;media=" + document.getElementById('pin').getElementsByTagName('img')[0].src + "&amp;description=h' onclick=\"window.open(this.href, 'windowName', 'width=600, height=400, left=24, top=24, scrollbars, resizable'); return false;\" rel='nofollow' target='_blank'><i class='fa fa-pinterest'/></a>");
   }
