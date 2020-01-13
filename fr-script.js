22a803eaa3b736754d6ac43524c05e068bd8da11
isbendev <54044125+isbendev@users.noreply.github.com>
2020-01-13 12:31:52 +0100

    Update fr-script.js

--- fr-script.js
+++ fr-script.js
@@ -32,7 +32,6 @@ function pageFullyLoaded(e) {
   onrendered: function(canvas) {
     $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
     //Canvas2Image.saveAsJPEG(canvas, 600, 600);
-    $('.pin').after("<a class='pin-art' href='http://pinterest.com/pin/create/button/?url=" + window.location.href + "&amp;media=" + document.getElementById('pin').getElementsByTagName('img')[0].src + "&amp;description=h' onclick=\"window.open(this.href, 'windowName', 'width=600, height=400, left=24, top=24, scrollbars, resizable'); return false;\" rel='nofollow' target='_blank'><i class='fa fa-pinterest'/></a>");
   }
   });
 }
