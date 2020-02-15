67aeece0d5db787b63390c58b7aa4eaafb30a44d
isbendev <54044125+isbendev@users.noreply.github.com>
2020-02-15 17:40:03 +0100

    Update wp-script.js

--- wp-script.js
+++ wp-script.js
@@ -1,6 +1,3 @@
-/*
-
-  
 var canvas2image=document.createElement('script');
 canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js");
@@ -10,13 +7,12 @@ var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
 html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js");
 document.getElementsByTagName("head")[0].appendChild(html2canvas); 
-*/
 
-  var style=document.createElement("link");
-  style.setAttribute("rel", "stylesheet");
-  style.setAttribute("type", "text/css");
-  style.setAttribute("href", "https://isbendev.github.io/isbendev/wp-style.css");
-  document.getElementsByTagName("head")[0].appendChild(style);
+var style=document.createElement("link");
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("type", "text/css");
+style.setAttribute("href", "https://isbendev.github.io/isbendev/wp-style.css");
+document.getElementsByTagName("head")[0].appendChild(style);
 
 document.addEventListener("DOMContentLoaded", function(event){
   $('.vce-related-box').prepend("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
