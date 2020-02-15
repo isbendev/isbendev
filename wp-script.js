c4f3fd2708d1b461f901df9e3b9abae153cd8f0a
isbendev <54044125+isbendev@users.noreply.github.com>
2020-02-15 17:11:02 +0100

    Update wp-script.js

--- wp-script.js
+++ wp-script.js
@@ -17,14 +17,14 @@ document.getElementsByTagName("head")[0].appendChild(html2canvas);
 */
 
 document.addEventListener("DOMContentLoaded", function(event){
-  $('.vce-related-box').append("fff");
+  $('.vce-related-box').prepend("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
+  $('.pinimg1').attr('src',$('.img-url').text());
+  $('.desctxt').text($('.article-title').text());
+  $('.copyrighttxt').text($('.site-url').text());
 });
 
 
-$('.vce-related-box').prepend("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
-$('.pinimg1').attr('src',$('.img-url').text());
-$('.desctxt').text($('.article-title').text());
-$('.copyrighttxt').text($('.site-url').text());
+
 
 /*
 $( document ).ready(function() { 
