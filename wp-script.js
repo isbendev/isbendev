54442680618060cf9dd068b9849e7ffbc4de010d
isbendev <54044125+isbendev@users.noreply.github.com>
2020-02-15 17:17:39 +0100

    Update wp-script.js

--- wp-script.js
+++ wp-script.js
@@ -12,14 +12,13 @@ html2canvas.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/html2can
 document.getElementsByTagName("head")[0].appendChild(html2canvas); 
 */
 
-document.addEventListener("DOMContentLoaded", function(event){
-  
   var style=document.createElement("link");
   style.setAttribute("rel", "stylesheet");
   style.setAttribute("type", "text/css");
   style.setAttribute("href", "https://isbendev.github.io/isbendev/wp-style.css");
   document.getElementsByTagName("head")[0].appendChild(style);
-  
+
+document.addEventListener("DOMContentLoaded", function(event){
   $('.entry-title').prepend("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
   $('.pinimg1').attr('src',$('.img-url').text());
   $('.desctxt').text($('.article-title').text());
