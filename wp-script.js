9908ea9da149e37b1949a2fda2c92819475d2344
isbendev <54044125+isbendev@users.noreply.github.com>
2020-02-15 18:32:53 +0100

    Update wp-script.js

--- wp-script.js
+++ wp-script.js
@@ -1,14 +1,9 @@
 
+/*
 var jquery=document.createElement('script');
 jquery.setAttribute("type","text/javascript");
 jquery.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
 document.getElementsByTagName("head")[0].appendChild(jquery); 
-
-var style=document.createElement("link");
-style.setAttribute("rel", "stylesheet");
-style.setAttribute("type", "text/css");
-style.setAttribute("href", "https://isbendev.github.io/isbendev/wp-style.css");
-document.getElementsByTagName("head")[0].appendChild(style);
 
 var html2canvas=document.createElement('script');
 html2canvas.setAttribute("type","text/javascript");
@@ -20,24 +15,25 @@ canvas2image.setAttribute("type","text/javascript");
 canvas2image.setAttribute("src", "https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js");
 document.getElementsByTagName("head")[0].appendChild(canvas2image);
 
+*/
+
 document.addEventListener("DOMContentLoaded", function(event){
 
+var style=document.createElement("link");
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("type", "text/css");
+style.setAttribute("href", "https://isbendev.github.io/isbendev/wp-style.css");
+document.getElementsByTagName("head")[0].appendChild(style);
+    
 $('.vce-related-box').prepend("<div class='pin'><div class='pinimg'><img class='post-thumbnail pinimg1' crossorigin='anonymous'/></div><div class='desc'><div class='desctxt'></div></div><div class='copyright'><div class='copyrighttxt'></div></div></div>");
 $('.pinimg1').attr('src',$('.img-url').text());
 $('.desctxt').text($('.article-title').text());
 $('.copyrighttxt').text($('.site-url').text());
 
-    
-html2canvas(document.querySelector(".pin")).then(canvas => {
-    //document.body.appendChild(canvas)
-    $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900)); 
-});
- 
-  /*
-  html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
-  onrendered: function(canvas) {
-    
+html2canvas(document.querySelector('.pin'), { logging: true, letterRendering: 1,  allowTaint: false, useCORS: true, 
+onrendered: function(canvas) {
+    $('.pin').html(Canvas2Image.convertToJPEG(canvas, 600, 900));
     }
-  });
-  */
+});
+  
 });
